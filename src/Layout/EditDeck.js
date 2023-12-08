bd2e18c7294c9d36c33d29666a8e8362c474c70c
Seth Greene <sethandrewg@gmail.com>
2023-12-08 16:22:50 -0500

    Worked on multiple paths and made bug fixes for /decks/:id/edit

---
+++ src/Layout/EditDeck.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react"
+import { Link, useHistory, useParams } from "react-router-dom"
+import { readDeck, updateDeck } from "../utils/api/index.js"
+
+function EditDeck() {
+
+    const { id } = useParams();
+
+    const history = useHistory();
+
+    const [deck, setDeck] = useState()
+
+    const [formData, setFormData] = useState();
+    
+    useEffect(() => {
+        const getAPIData = async () => {
+            const getData = await readDeck(id);
+            setDeck(getData);
+        }
+        getAPIData();
+    }, [])
+
+    useEffect (() => {
+        if (deck) {
+            const initialFormData = {
+                id: id,
+                name: deck.name,
+                description: deck.description
+            }
+            setFormData(initialFormData)
+        }
+    }, [deck])
+    
+    if (!formData) return <p>loading</p>
+    
+    const changeHandler = ({ target }) => {
+        setFormData({
+            ...formData,
+            [target.name]: target.value
+        })
+    }
+    
+    const submitHandler = async (event) => {
+        event.preventDefault()
+        const data = await updateDeck(formData)
+        history.push("/")
+    }
+    
+    return (
+        <>
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                    <li className="breadcrumb-item"><Link to="/">{ deck.name }</Link></li>
+                    <li className="breadcrumb-item active" aria-current="page">Edit</li>
+                </ol>
+            </nav>
+            <h3>Create Deck</h3>
+            <form onSubmit={submitHandler}>
+                <p>Name</p>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Deck Name"
+                    required
+                    value={formData.name}
+                    onChange={changeHandler}
+                />
+                <hr/>
+                <p>Description</p>
+                <textarea
+                    type="text"
+                    name="description"
+                    id="description"
+                    placeholder="Add a Deck Description"
+                    required
+                    value={formData.description}
+                    onChange={changeHandler}
+                />
+                <hr/>
+                <button type="button" className="btn btn-secondary" onClick={() => history.push("/")}>Cancel</button>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </>
+        
+    )
+}
+
+export default EditDeck;
